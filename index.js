55dbe7fb2af75cfb11232958537f5c81b0a28b66
Samuel Reed <samuel.trace.reed@gmail.com>
2017-05-03 16:01:25 -0500

    De-es6ify this one weird trick; uglifyJS hates this

--- index.js
+++ index.js
@@ -18,14 +18,19 @@ module.exports = {
 
   // Same as _.merge, but merges array arguments together (deeper merge).
   // If given an explicit null, will overwrite with null.
-  mergeWithArrays(...args/*: Object[] */)/*: Object */ {
+  mergeWithArrays()/*: Object */ {
+    const args = new Array(arguments.length);
+    for (let i = 0; i < arguments.length; i++) {
+      args[i] = arguments[i];
+    }
+
     args.push(function(a, b) {
       // Allow using null to overwrite
       if (_.isArray(a) && b === null) return null;
       // Concat arrays of options together
       if (_.isArray(a) && b) return a.concat(b);
     });
-    return _.mergeWith(...args);
+    return _.mergeWith.apply(_, args);
   }
 
 };
